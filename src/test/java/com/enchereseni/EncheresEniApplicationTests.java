d5b29262eae58321b985208b7385781c1947ee90
llesueur2024 <317Y17@gmail.com>
2025-01-27 10:39:50 +0100

    First Commit

---
+++ src/test/java/com/enchereseni/EncheresEniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enchereseni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncheresEniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
