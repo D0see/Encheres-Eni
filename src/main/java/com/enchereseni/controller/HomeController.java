7aca4cf2d14b76f5639817f91b7255c25bbe4c37
Federico2235 <fedeand3@gmail.com>
2025-01-27 15:44:19 +0100

    rename in english

--- src/main/java/com/enchereseni/controller/HomeController.java
+++ src/main/java/com/enchereseni/controller/HomeController.java
@@ -4,12 +4,11 @@ package com.enchereseni.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 //@RequestMapping("/encheres")
-public class EncheresController {
+public class HomeController {
 
 
 @GetMapping("/encheres")
