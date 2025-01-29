6ba4566e5f55ab49bb2d0113598c0aafeb7a4127
llesueur2024 <317Y17@gmail.com>
2025-01-29 10:14:50 +0100

    first controller impl

--- src/main/java/com/enchereseni/controller/UserController.java
+++ src/main/java/com/enchereseni/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.enchereseni.controller;
 
+import com.enchereseni.bll.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+@Controller
+@RequestMapping("/users")
 public class UserController {
+
+    private UserService userService;
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public String me(Principal principal, Model model) {
+        var inSessionName = principal.getName();
+        userService.getUsers().stream().filter(user -> user.getUsername().equals(inSessionName)).findFirst().ifPresent(user -> {
+            model.addAttribute("user", user);
+        });
+        return "affichageUser";
+    }
+
 }
