49cd4df033a3561a1d6641dce3eedfc57fffe7fa
llesueur2024 <317Y17@gmail.com>
2025-01-31 16:44:46 +0100

    wokring on auction implementation

--- src/main/java/com/enchereseni/bll/AuctionServiceImpl.java
+++ src/main/java/com/enchereseni/bll/AuctionServiceImpl.java
@@ -45,12 +45,17 @@ public class AuctionServiceImpl implements AuctionService {
 
     @Override
     public List<Auction> getAuctionsByUsername(String username) {
-        return List.of();
+        return getAllAuctions().stream().filter(auction -> {
+            return auction.getUser().getUsername().equals(username);
+        }).toList();
+
     }
 
     @Override
     public List<Auction> getAuctionsByItem(ItemSold itemSold) {
-        return List.of();
+        return getAllAuctions().stream().filter(auction -> {
+            return auction.getItemSold().getItemId() == itemSold.getItemId();
+        }).toList();
     }
 
 }
