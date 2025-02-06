e5bf52ed2deb56d4e0bff0e52d892d009ba736d8
Celeste CAVALLIN <celeste.cavallin2024@campus-eni.fr>
2025-02-06 12:15:48 +0100

    last fix

--- src/main/java/com/enchereseni/bll/ItemServiceImpl.java
+++ src/main/java/com/enchereseni/bll/ItemServiceImpl.java
@@ -82,87 +82,87 @@ public class ItemServiceImpl implements ItemService {
         itemSoldDAO.removeItemSoldById(item.getItemId());
     }
 
-    @Transactional
-    public void creationItemSold(ItemSold itemSold,PickUp pickUp ) {
-        BusinessException be = new BusinessException();
-        boolean isValid = true;
-        isValid &= validerItemName(itemSold.getName(), be);
-        isValid &= validerItemDescription(itemSold.getDescription(), be);
-        isValid &= validerItemBeginningAuctionDate(itemSold.getBeginningAuctionDate(), be);
-        isValid &= validerItemEndingAuctionDate(itemSold.getEndingAuctionDate(), be);
-        isValid &= validerItemCategory(itemSold.getCategory(), be);
-        isValid &= validerItemAddress(pickUp.getAddress(), be);
-        isValid &= validerItemZipCode(pickUp.getZipCode(), be);
-        isValid &= validerItemCity(pickUp.getCity(), be);
-
-        if (isValid) {
-            try{
-                itemSoldDAO.createItemSold(itemSold);
-            }catch (DataAccessException e) {
-                be.add(BusinessCode.BLL_ITEM_CREER_ERREUR);
-                throw be;
-            }
-            } else {
-                throw be;
-        }
-    }
-
-    //NAME
-    private boolean validerItemName(String itemName, BusinessException be){
-    if (itemName.equals("")) {
-        be.add(BusinessCode.VALIDATION_ITEM_NAME_EMPTY);
-        return false;
-    }
-    if (itemName.length() > 30) {
-        be.add(BusinessCode.VALIDATION_ITEM_NAME_SIZE);
-        return false;
-    }
-    return true;
-    }
-
-    //DESCRIPTION
-    private boolean validerItemDescription(String itemDescription, BusinessException be){
-    if (itemDescription.equals("")) {
-        be.add(BusinessCode.VALIDATION_ITEM_DESCRIPTION_ITEM);
-        return false;
-    }
-    if (itemDescription.length() > 300) {
-        be.add(BusinessCode.VALIDATION_ITEM_DESCRIPTION_SIZE);
-    }
-    return true;
-    }
-
-    //BEGINNING ACTION DATE
-    private boolean validerItemBeginningAuctionDate(LocalDate date, BusinessException be){
-        if (date == null){
-            be.add(BusinessCode.VALIDATION_ITEM_NULL);
-            return false;
-        }
-
-    if (date.isBefore(LocalDate.now())) {
-        be.add(BusinessCode.VALIDATION_ITEM_TOOEARLY);
-    }
-
-    return true;
-    }
-
-    //ENDING AUCTION DATE
-    private boolean validerItemEndingAuctionDate(LocalDate beginningAuctionDate, LocalDate endingAuctionDate, BusinessException be){
-    //TO DO validation pour que la fin ne soit pas avant le debut enchere
-    if (endingAuctionDate.isBefore(beginningAuctionDate)) {
-        be.add(BusinessCode.VALIDATION_ITEM_TOO_TOOEARLY);
-    }
-
-    }
-
-    //CATEGORY
-    private boolean validerItemCategory(Category category, BusinessException be){
-    if (category == null) {
-        be.add(BusinessCode.VALIDATION_ITEM_CATEGORY_NULL);
-        return false;
-    }
-    return true;
-    }
+//    @Transactional
+//    public void creationItemSold(ItemSold itemSold,PickUp pickUp ) {
+//        BusinessException be = new BusinessException();
+//        boolean isValid = true;
+//        isValid &= validerItemName(itemSold.getName(), be);
+//        isValid &= validerItemDescription(itemSold.getDescription(), be);
+//        isValid &= validerItemBeginningAuctionDate(itemSold.getBeginningAuctionDate(), be);
+//        isValid &= validerItemEndingAuctionDate(itemSold.getEndingAuctionDate(), be);
+//        isValid &= validerItemCategory(itemSold.getCategory(), be);
+//        isValid &= validerItemAddress(pickUp.getAddress(), be);
+//        isValid &= validerItemZipCode(pickUp.getZipCode(), be);
+//        isValid &= validerItemCity(pickUp.getCity(), be);
+//
+//        if (isValid) {
+//            try{
+//                itemSoldDAO.createItemSold(itemSold);
+//            }catch (DataAccessException e) {
+//                be.add(BusinessCode.BLL_ITEM_CREER_ERREUR);
+//                throw be;
+//            }
+//            } else {
+//                throw be;
+//        }
+//    }
+//
+//    //NAME
+//    private boolean validerItemName(String itemName, BusinessException be){
+//    if (itemName.equals("")) {
+//        be.add(BusinessCode.VALIDATION_ITEM_NAME_EMPTY);
+//        return false;
+//    }
+//    if (itemName.length() > 30) {
+//        be.add(BusinessCode.VALIDATION_ITEM_NAME_SIZE);
+//        return false;
+//    }
+//    return true;
+//    }
+//
+//    //DESCRIPTION
+//    private boolean validerItemDescription(String itemDescription, BusinessException be){
+//    if (itemDescription.equals("")) {
+//        be.add(BusinessCode.VALIDATION_ITEM_DESCRIPTION_ITEM);
+//        return false;
+//    }
+//    if (itemDescription.length() > 300) {
+//        be.add(BusinessCode.VALIDATION_ITEM_DESCRIPTION_SIZE);
+//    }
+//    return true;
+//    }
+//
+//    //BEGINNING ACTION DATE
+//    private boolean validerItemBeginningAuctionDate(LocalDate date, BusinessException be){
+//        if (date == null){
+//            be.add(BusinessCode.VALIDATION_ITEM_NULL);
+//            return false;
+//        }
+//
+//    if (date.isBefore(LocalDate.now())) {
+//        be.add(BusinessCode.VALIDATION_ITEM_TOOEARLY);
+//    }
+//
+//    return true;
+//    }
+//
+//    //ENDING AUCTION DATE
+//    private boolean validerItemEndingAuctionDate(LocalDate beginningAuctionDate, LocalDate endingAuctionDate, BusinessException be){
+//    //TO DO validation pour que la fin ne soit pas avant le debut enchere
+//    if (endingAuctionDate.isBefore(beginningAuctionDate)) {
+//        be.add(BusinessCode.VALIDATION_ITEM_TOO_TOOEARLY);
+//    }
+//
+//    }
+//
+//    //CATEGORY
+//    private boolean validerItemCategory(Category category, BusinessException be){
+//    if (category == null) {
+//        be.add(BusinessCode.VALIDATION_ITEM_CATEGORY_NULL);
+//        return false;
+//    }
+//    return true;
+//    }
 
 
 }
